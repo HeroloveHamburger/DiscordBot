1e865932ca852e5caf76ee386a03e62d38fbed24
HeroloveHamburger <46862908+HeroloveHamburger@users.noreply.github.com>
2021-11-29 15:51:06 +0800

    Create StatusClass.py

---
+++ StatusClass.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class statusCreate():
+    def __init__(self, id,name, health = 4000,card = 40):
+        self.id = id
+        self.name = name
+        self.health = health
+        self.card = card
+
